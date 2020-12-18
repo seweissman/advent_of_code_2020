fb61c197d485716bb0ab9e89e08fe7be3ec56f2c
Sarah Weissman <sweissman@stsci.edu>
2020-12-17 21:20:37 -0500

    Day 17 part 2

--- day17/day17.py
+++ day17/day17.py
@@ -1,69 +1,92 @@
 
 from collections import defaultdict
 
+def adj_list_offset(n, l=None):
+    """Return a generator with offsets"""
+    if l is None:
+        l = [[]]
+    if n == 0:
+        return l
+    else:
+        return adj_list_offset(n-1, ([i, *c] for c in l for i in [-1, 0, 1]))
+
+
+def adj_list(c, l=None):
+    """Return a generator with offsets"""
+    if l is None:
+        l = [()]
+    if len(c) == 0:
+        return l
+    else:
+        return adj_list(c[0:-1], ((c[-1] + i,) + subc for subc in l for i in [-1, 0, 1]))
+
+
+def range_coord(c_min, c_max, l=None):
+    """Return a generator with offsets"""
+    if l is None:
+        l = [()]
+    if len(c_min) == 0:
+        return l
+    else:
+        return range_coord(c_min[0:-1], c_max[0:-1], ((c_min[-1] + i,) + subc for subc in l for i in range(0, c_max[-1]-c_min[-1]+1)))
+
+
 def count_active_neighbors(state, c):
     active_ct = 0
-    # print(f"Checking for {c}")
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            for k in range(-1, 2):
-                if (i, j, k) != (0, 0, 0):
-                    c_neighbor = (c[0] + i, c[1] + j, c[2] + k)
-                    # print(f"State for neighbor {c_neighbor} = {state[c_neighbor]}")
-                    if state[c_neighbor] == "#":
-                        active_ct += 1
+    for c_neighbor in adj_list(c):
+        if c_neighbor != c:
+            # print(f"State for neighbor {c_neighbor} = {state[c_neighbor]}")
+            if state[c_neighbor] == "#":
+                active_ct += 1
 
-    # print(c, active_ct)
     return active_ct
 
-def get_range(state):
-    min_range = [None for i in range(0, 3)]
-    max_range = [None for i in range (0, 3)]
+def get_range(state, n):
+    min_range = [None for i in range(0, n)]
+    max_range = [None for i in range (0, n)]
     for c in state:
-        # print(f"{c} in state")
-        # print("Before", min_range, max_range)
-        for i in range(0, 3):
+        for i in range(0, n):
             if min_range[i] is None or c[i] < min_range[i]:
                 min_range[i] = c[i]
             if max_range[i] is None or c[i] > max_range[i]:
                 max_range[i] = c[i]
-        # print("After", min_range, max_range)
     return min_range, max_range
 
+def print_state(state, n):
+    min_range, max_range = get_range(state, n)
+    for c in range_coord(min_range[2:], max_range[2:]):
+        print(f"c={c}")
+        print_2d_state(state,min_range[0:2], max_range[0:2],c)
 
-def print_state(state):
-    min_range, max_range = get_range(state)
-    print("range_print:", min_range, max_range)
-    for z in range(min_range[2]-1, max_range[2]+2):
-        print(f"z={z}")
-        for y in range(min_range[1]-1, max_range[1]+2):
-            line = ""
-            for x in range(min_range[0]-1, max_range[0]+1):
-                line += state[(x,y,z)]
-            print(line)
+def print_2d_state(state, min_range, max_range, c=()):
+    line = ["." for i in range(max_range[0] - min_range[0]+1)]
+    lines = [line.copy() for i in range(max_range[1] - min_range[1]+1)]
+    for d in range_coord(min_range[0:2], max_range[0:2]):
+        lines[d[1]][d[0]] = state[d+c]
+    print("\n".join(["".join(line) for line in lines]))
 
 def count_active(state):
     return len([v for v in state.values() if v == "#"])
 
 def step(state):
+    seen = set()
     new_state = state.copy()
-    min_range, max_range = get_range(state)
-    # print("range step:", min_range, max_range)
-    for i in range(min_range[0]-1, max_range[0]+2):
-        for j in range(min_range[1] - 1, max_range[1] + 2):
-            for k in range(min_range[2] - 1, max_range[2] + 2):
-                c = (i, j, k)
-                active_ct = count_active_neighbors(state, c)
-                if state[c] == "#" and (active_ct < 2 or active_ct > 3):
-                    new_state[c] = "."
-                if state[c] == "." and active_ct == 3:
-                    new_state[c] = "#"
-
-
+    for c in list(state.keys()).copy():
+        if state[c] == "#":
+            for c_adj in adj_list(c):
+                if c_adj in seen:
+                    continue
+                seen.add(c_adj)
+                active_ct = count_active_neighbors(state, c_adj)
+                if state[c_adj] == "#" and (active_ct < 2 or active_ct > 3):
+                    new_state[c_adj] = "."
+                if state[c_adj] == "." and active_ct == 3:
+                    new_state[c_adj] = "#"
     return new_state
 
+
 if __name__ == "__main__":
-    with open("input.txt") as input:
+    with open("input-test.txt") as input:
         lines = input.readlines()
         lines = [line.strip() for line in lines]
 
@@ -74,13 +97,30 @@ if __name__ == "__main__":
         for x in range(0, len(lines[0])):
             state[(x, y, 0)] = lines[y][x]
 
-    print_state(state)
+    # print_2d_state(state, (0, 0), (2,2), c=(0,))
+    print_state(state, 3)
     for n in range(0, 6):
         state = step(state)
         print(f"After {n+1} cycles:")
-        # print_state(state)
         print(count_active(state))
 
+    # part 2
+
+    state = defaultdict(lambda: ".")
+
+    for y in range(0, len(lines)):
+        line = lines[y]
+        for x in range(0, len(lines[0])):
+            state[(x, y, 0, 0)] = lines[y][x]
+
+    # print_state(state, 4)
+
+    for n in range(0, 6):
+        state = step(state)
+        print(f"After {n+1} cycles:")
+        print(count_active(state))
+
+    # print_state(state, 4)
 
 
 
