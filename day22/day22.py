82bbb3dd59c7e6805bc2c976b5799847ce3e869e
Sarah Weissman <sweissman@stsci.edu>
2020-12-22 09:39:00 -0500

    code cleanup

--- day22/day22.py
+++ day22/day22.py
@@ -3,7 +3,26 @@ import itertools
 from datetime import datetime
 
 
+def combat(decks):
+    """Regular combat"""
+    while True:
+        if not decks[0]:
+            return 1
+        if not decks[1]:
+            return 0
+        # print(decks)
+        combat_cards = [deck.popleft() for deck in decks1]
+        # print("Combat cards:", combat_cards)
+        if combat_cards[0] > combat_cards[1]:
+            for combat_card in combat_cards:
+                decks1[0].append(combat_card)
+        else:
+            for combat_card in combat_cards[::-1]:
+                decks1[1].append(combat_card)
+
+
 def recursive_combat(decks):
+    """Recursive Combat"""
     seen_decks = set()
     #print(f"recursive_combat {decks}")
     while True:
@@ -33,6 +52,12 @@ def recursive_combat(decks):
                 decks[1].append(combat_card)
 
 
+def score_deck(deck):
+    score = 0
+    for i, card in enumerate(deck):
+        score += (len(deck) - i)*card
+    return score
+
 
 if __name__ == "__main__":
     with open("input.txt") as input:
@@ -50,43 +75,20 @@ if __name__ == "__main__":
             if line != "":
                 deck.append(int(line))
     decks.append(deck)
-    print(decks)
 
+    # part 2
     decks1 = [deque(deck) for deck in decks]
-    print(decks)
-    while(True):
-        # print(decks)
-        combat_cards = [deck.popleft() for deck in decks1]
-        # print("Combat cards:", combat_cards)
-        if combat_cards[0] > combat_cards[1]:
-            for combat_card in combat_cards:
-                decks1[0].append(combat_card)
-        else:
-            for combat_card in combat_cards[::-1]:
-                decks1[1].append(combat_card)
-        if deque([]) in decks1:
-            break
-
-    winner = [deck for deck in decks1 if deck != deque([])][0]
-    print("Winner:", winner)
-
-    score = 0
-    for i, card in enumerate(winner):
-        score += (len(winner) -i)*card
-
+    winner = combat(decks1)
+    score = score_deck(decks1[winner])
     print("Score1: ", score)
 
-    start = datetime.now()
     # part 2
+    start = datetime.now()
     decks2 = [deque(deck) for deck in decks]
     winner = recursive_combat(decks2)
-    print(winner)
-    print(decks2[winner])
-    score = 0
-    for i, card in enumerate(decks2[winner]):
-        score += (len(decks2[winner]) - i)*card
-
+    score = score_deck(decks2[winner])
     print("Score2: ", score)
+
     end = datetime.now()
-    print("Time: ", end - start)
+    print("Part 2 time: ", end - start)
 
