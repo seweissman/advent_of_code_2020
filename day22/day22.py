31c557acce96f9aa0a15742617a13be98b4d3dd2
Sarah Weissman <sweissman@stsci.edu>
2020-12-22 09:17:35 -0500

    Implementation with deque

--- day22/day22.py
+++ day22/day22.py
@@ -1,5 +1,10 @@
+from collections import deque
+import itertools
+from datetime import datetime
+
+
 def recursive_combat(decks):
-    seen_decks = []
+    seen_decks = set()
     #print(f"recursive_combat {decks}")
     while True:
         #print(f"Decks {decks}")
@@ -7,24 +12,25 @@ def recursive_combat(decks):
             return 1
         if not decks[1]:
             return 0
-        if decks in seen_decks:
+        if str(decks) in seen_decks:
             #print("Seen decks", decks)
             return 0 # player 1 in winner
-        seen_decks.append([deck.copy() for deck in decks])
-        combat_cards = [deck.pop() for deck in decks]
+        seen_decks.add(str(decks))
+        combat_cards = [deck.popleft() for deck in decks]
         # for i in range(2):
         #     print(f"Player {i} plays {combat_cards[i]}")
         can_play = [len(decks[i]) >= combat_cards[i] for i in range(2)]
         if can_play[0] and can_play[1]:
-            winner = recursive_combat([decks[i][-combat_cards[i]:] for i in range(2)])
+            new_decks = [deque(itertools.islice(decks[i], 0, combat_cards[i])) for i in range(2)]
+            winner = recursive_combat(new_decks)
         else:
             winner = combat_cards.index(max(combat_cards))
         if winner == 0:
             for combat_card in combat_cards:
-                decks[0].insert(0, combat_card)
+                decks[0].append(combat_card)
         else:
             for combat_card in combat_cards[::-1]:
-                decks[1].insert(0, combat_card)
+                decks[1].append(combat_card)
 
 
 
@@ -42,42 +48,45 @@ if __name__ == "__main__":
             deck = []
         else:
             if line != "":
-                deck.insert(0, int(line))
+                deck.append(int(line))
     decks.append(deck)
     print(decks)
 
-    decks1 = [deck.copy() for deck in decks]
-    print(decks1)
+    decks1 = [deque(deck) for deck in decks]
+    print(decks)
     while(True):
         # print(decks)
-        combat_cards = [deck.pop() for deck in decks1]
+        combat_cards = [deck.popleft() for deck in decks1]
         # print("Combat cards:", combat_cards)
         if combat_cards[0] > combat_cards[1]:
             for combat_card in combat_cards:
-                decks1[0].insert(0, combat_card)
+                decks1[0].append(combat_card)
         else:
             for combat_card in combat_cards[::-1]:
-                decks1[1].insert(0, combat_card)
-        if [] in decks1:
+                decks1[1].append(combat_card)
+        if deque([]) in decks1:
             break
 
-    winner = [deck for deck in decks1 if deck != []][0]
+    winner = [deck for deck in decks1 if deck != deque([])][0]
     print("Winner:", winner)
 
     score = 0
     for i, card in enumerate(winner):
-        score += (i + 1)*card
+        score += (len(winner) -i)*card
 
     print("Score1: ", score)
 
+    start = datetime.now()
     # part 2
-
-    winner = recursive_combat(decks)
+    decks2 = [deque(deck) for deck in decks]
+    winner = recursive_combat(decks2)
     print(winner)
-    print(decks[winner])
+    print(decks2[winner])
     score = 0
-    for i, card in enumerate(decks[winner]):
-        score += (i + 1)*card
+    for i, card in enumerate(decks2[winner]):
+        score += (len(decks2[winner]) - i)*card
 
     print("Score2: ", score)
+    end = datetime.now()
+    print("Time: ", end - start)
 
