5a67809fed0f7a99b88b72fb597446014a2c86c5
Sarah Weissman <sweissman@stsci.edu>
2020-12-23 14:18:04 -0500

    some cleanup

--- day23/day23.py
+++ day23/day23.py
@@ -2,7 +2,13 @@
 class Cup:
     def __init__(self, label):
         self.label = label
-        self.nextCup = None
+        self.next_cup = None
+
+    def __next__(self):
+        return self.next_cup
+
+    def set_next(self, cup):
+        self.next_cup = cup
 
     def __repr__(self):
         return f"Cup({self.label})"
@@ -12,33 +18,33 @@ def print_cups(cup):
     cup_str = ""
     while True:
         cup_str += str(cup.label)
-        cup = cup.nextCup
+        cup = next(cup)
         if cup.label == start_label:
             break
         cup_str += " "
     print(cup_str)
 
-def make_move(current_cup: Cup, cups_map, nCups):
+def make_move(current_cup: Cup, cups_map, n_cups):
     # print_cups(current_cup)
-    following_cup = current_cup.nextCup
-    current_cup.nextCup = following_cup.nextCup.nextCup.nextCup
-    following_cup.nextCup.nextCup.nextCup = None
-    following_cup_labels = [following_cup.label, following_cup.nextCup.label, following_cup.nextCup.nextCup.label]
+    following_cup = next(current_cup)
+    current_cup.set_next(next(next(next(following_cup))))
+    next(next(following_cup)).set_next(None)
+    following_cup_labels = [following_cup.label, next(following_cup).label, next(next(following_cup)).label]
     # print(f"pick up: {following_cup_labels}")
     destination_label = current_cup.label - 1
     if destination_label == 0:
-        destination_label = nCups
+        destination_label = n_cups
 
     while destination_label in following_cup_labels:
         destination_label -= 1
         if destination_label == 0:
-            destination_label = nCups
+            destination_label = n_cups
     # print("destination:", destination_label, "\n")
 
     destination_cup = cups_map[destination_label]
-    after_cup = destination_cup.nextCup
-    destination_cup.nextCup = following_cup
-    following_cup.nextCup.nextCup.nextCup = after_cup
+    after_cup = next(destination_cup)
+    destination_cup.set_next(following_cup)
+    next(next(following_cup)).set_next(after_cup)
 
 def make_cups(input, cups_map, extend=None):
     first_cup = None
@@ -52,7 +58,7 @@ def make_cups(input, cups_map, extend=None):
         cup = Cup(label)
         cups_map[label] = cup
         if prev_cup:
-            prev_cup.nextCup = cup
+            prev_cup.set_next(cup)
         # Set current cup to the first cup
         if not first_cup:
             first_cup = cup
@@ -63,10 +69,10 @@ def make_cups(input, cups_map, extend=None):
         for i in range(max_label + 1, extend + 1):
             cup = Cup(i)
             cups_map[i] = cup
-            prev_cup.nextCup = cup
+            prev_cup.set_next(cup)
             prev_cup = cup
 
-    cup.nextCup = first_cup
+    cup.set_next(first_cup)
     return first_cup
 
 if __name__ == "__main__":
@@ -76,23 +82,22 @@ if __name__ == "__main__":
 
     cups_map = {}
     first_cup = make_cups(input, cups_map)
-    # Make this list circular
     print_cups(first_cup)
 
     curr_cup = first_cup
     for _ in range(100):
         make_move(curr_cup, cups_map, 9)
-        curr_cup = curr_cup.nextCup
+        curr_cup = next(curr_cup)
         # print(cups, index)
 
     cup1 = cups_map[1]
 
     print_cups(cup1)
     answer = ""
-    cup = cup1.nextCup
+    cup = next(cup1)
     while cup.label != 1:
         answer += str(cup.label)
-        cup = cup.nextCup
+        cup = next(cup)
 
     print("Answer 1", answer)
 
@@ -105,10 +110,10 @@ if __name__ == "__main__":
     curr_cup = first_cup
     for i in range(10000000):
         make_move(curr_cup, cups_map, 1000000)
-        curr_cup = curr_cup.nextCup
+        curr_cup = next(curr_cup)
         if i % 1000000 == 0:
             print("At move: ", i)
         # print(cups, index)
 
     cup1 = cups_map[1]
-    print("Answer2", cup1.nextCup.label*cup1.nextCup.nextCup.label)
+    print("Answer2", next(cup1).label*next(next(cup1)).label)
