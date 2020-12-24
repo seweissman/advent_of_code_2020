89f74cd71c0240da19ad22dafc5b12c2afc0f958
Sarah Weissman <sweissman@stsci.edu>
2020-12-24 12:27:53 -0500

    Moving some methods to HexTile

--- day24/day24.py
+++ day24/day24.py
@@ -11,6 +11,8 @@ def reverse(dir):
 class HexTile:
     ct = 0
     tile_list = []
+    ref_tile = None
+    tiles_to_set = None
 
     def __init__(self):
         self.nw = None
@@ -21,6 +23,10 @@ class HexTile:
         self.sw = None
         self.color = "white"
         self.id = HexTile.ct
+        if HexTile.ct == 0:
+            HexTile.ref_tile = self
+            HexTile.tiles_to_set = deque([HexTile.ref_tile])
+
         HexTile.ct += 1
         HexTile.tile_list.append(self)
 
@@ -52,6 +58,103 @@ class HexTile:
             if tile.color == "black":
                 ct += 1
         return ct
+
+    @classmethod
+    def change_grid(cls):
+        # Save the current grid state in a map so we can make our changes
+        # in place
+        tile_color_map = {}
+        for tile in cls.tile_list:
+            tile_color_map[tile.id] = tile.color
+
+        # Iterate over tiles and flip according to rules
+        for tile in cls.tile_list:
+            black_ct = 0
+            white_ct = 0
+            for adj_tile in tile.adj_tiles():
+                if adj_tile is None:
+                    white_ct += 1
+                else:
+                    if tile_color_map[adj_tile.id] == "white":
+                        white_ct += 1
+                    else:
+                        black_ct += 1
+            if tile.color == "black" and (black_ct == 0 or black_ct > 2):
+                tile.flip()
+            if tile.color == "white" and black_ct == 2:
+                tile.flip()
+
+    @classmethod
+    def find_tile(cls, dir_str):
+        """
+        Given a direction string, e.g. wenwwweseeeweswwwnwwe, and a starting tile, return
+        tile in the grid we are on after following the directions.
+        """
+        i = 0
+        curr_tile = cls.ref_tile
+        while i < len(dir_str):
+            if dir_str[i] == "s" or dir_str[i] == "n":
+                dir = dir_str[i:i + 2]
+                i = i + 2
+            else:
+                dir = dir_str[i]
+                i = i + 1
+            # print(dir)
+            new_tile = curr_tile.get_tile(dir)
+            assert (new_tile is not None)
+            curr_tile = new_tile
+        return curr_tile
+
+    @classmethod
+    def grow_grid(cls, n):
+        while True:
+            if n == 0:
+                return
+            ref_tile = cls.tiles_to_set.popleft()
+
+            ne_tile = ref_tile.get_tile("ne")
+            if not ne_tile:
+                ne_tile = HexTile()
+                ref_tile.set_tile("ne", ne_tile)
+                cls.tiles_to_set.append(ne_tile)
+
+            e_tile = ref_tile.get_tile("e")
+            if not e_tile:
+                e_tile = HexTile()
+                ref_tile.set_tile("e", e_tile)
+                cls.tiles_to_set.append(e_tile)
+            ne_tile.set_tile("se", e_tile)
+
+            se_tile = ref_tile.get_tile("se")
+            if not se_tile:
+                se_tile = HexTile()
+                ref_tile.set_tile("se", se_tile)
+                cls.tiles_to_set.append(se_tile)
+            e_tile.set_tile("sw", se_tile)
+
+            sw_tile = ref_tile.get_tile("sw")
+            if not sw_tile:
+                sw_tile = HexTile()
+                ref_tile.set_tile("sw", sw_tile)
+                cls.tiles_to_set.append(sw_tile)
+            se_tile.set_tile("w", sw_tile)
+
+            w_tile = ref_tile.get_tile("w")
+            if not w_tile:
+                w_tile = HexTile()
+                ref_tile.set_tile("w", w_tile)
+                cls.tiles_to_set.append(w_tile)
+            sw_tile.set_tile("nw", w_tile)
+
+            nw_tile = ref_tile.get_tile("nw")
+            if not nw_tile:
+                nw_tile = HexTile()
+                ref_tile.set_tile("nw", nw_tile)
+                cls.tiles_to_set.append(nw_tile)
+
+            w_tile.set_tile("ne", nw_tile)
+            nw_tile.set_tile("e", ne_tile)
+            n -= 1
 
 
 def build_grid(start_tile, n):
@@ -109,29 +212,7 @@ def build_grid(start_tile, n):
         nw_tile.set_tile("e", ne_tile)
         n -= 1
 
-def change_grid(tile_list):
-    # Save the current grid state in a map so we can make our changes
-    # in place
-    tile_color_map = {}
-    for tile in tile_list:
-        tile_color_map[tile.id] = tile.color
 
-    # Iterate over tiles and flip according to rules
-    for tile in tile_list:
-        black_ct = 0
-        white_ct = 0
-        for adj_tile in tile.adj_tiles():
-            if adj_tile is None:
-                white_ct +=1
-            else:
-                if tile_color_map[adj_tile.id] == "white":
-                    white_ct += 1
-                else:
-                    black_ct += 1
-        if tile.color == "black" and (black_ct == 0 or black_ct > 2):
-            tile.flip()
-        if tile.color == "white" and black_ct == 2:
-            tile.flip()
 
 
 def find_tile(ref_tile, dir_str):
@@ -160,25 +241,25 @@ if __name__ == "__main__":
         lines = input.readlines()
         lines = [line.strip() for line in lines]
 
-    ring = 0
-    pos = 0
     ref_tile = HexTile()
+    #ref_tile = HexTile()
 
-    tile_list = deque([ref_tile])
+    #tile_list = deque([ref_tile])
     # After a bit of experimenting we determine that this size grid is big enough
     # to solve the puzzle. For a bigger puzzle we'd have to increase the size of the grid
-    build_grid(ref_tile, 15000)
+    HexTile.grow_grid(15000)
+    #build_grid(ref_tile, 15000)
 
     # Some sanity checks
-    tile = find_tile(ref_tile, "nwse")
+    tile = HexTile.find_tile("nwse")
     assert(tile.id == ref_tile.id)
-    tile = find_tile(ref_tile, "nwwswee")
+    tile = HexTile.find_tile("nwwswee")
     assert(tile.id == ref_tile.id)
 
     # Part 1
     for line in lines:
         i = 0
-        tile = find_tile(ref_tile, line)
+        tile = HexTile.find_tile(line)
         tile.flip()
 
     print("Answer: ", HexTile.count_black())
@@ -187,7 +268,7 @@ if __name__ == "__main__":
 
     i = 0
     while i < 100:
-        change_grid(HexTile.tile_list)
+        HexTile.change_grid()
         i += 1
         if i % 10 == 0:
             print(i)
